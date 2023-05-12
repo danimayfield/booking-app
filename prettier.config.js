e798cfc9460287e8410d7aeff360fa933a64d237
Dani Mayfield <dani@neon.cc>
2023-05-12 17:15:00 -0400

    feat: configure tailwind & prettier

---
+++ prettier.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  trailingComma: "es5",
+  singleQuote: true,
+  endOfLine: "auto",
+  tailwindConfig: "./tailwind.config.js",
+  plugins: [require("prettier-plugin-tailwindcss")],
+};
