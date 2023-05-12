8250cb863746c9bf6d1db3aad197dd0b293ea6cd
Dani Mayfield <dani@neon.cc>
2023-05-12 17:15:38 -0400

    feat: add Nav & Container component

---
+++ src/features/layout/Nav.tsx
@@ -0,0 +1,68 @@
+'use client';
+import routes from '@/shared/routes';
+import React, { useState } from 'react';
+import { BsCalendarPlus, BsCalendarWeek } from 'react-icons/bs';
+import { HiOutlineHome } from 'react-icons/hi';
+import { AnimatePresence, motion } from 'framer-motion';
+
+type NavItem = {
+  route: string;
+  name: string;
+  icon: React.ReactNode;
+};
+
+const navItems: NavItem[] = [
+  {
+    route: routes.home,
+    icon: <HiOutlineHome size="1.5rem" className="ml-1" />,
+    name: 'Home',
+  },
+  {
+    route: routes.newBooking,
+    icon: <BsCalendarPlus size="1.3rem" className="ml-1" />,
+    name: 'Add Booking',
+  },
+  {
+    route: routes.viewBookings,
+    icon: <BsCalendarWeek size="1.3rem" className="ml-1" />,
+    name: 'View All Bookings',
+  },
+];
+
+const NavItem = ({ item }: { item: NavItem }) => {
+  const [isHovered, setIsHovered] = useState(false);
+  return (
+    <a
+      href={item.route}
+      className="relative"
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <div className="block h-full w-full transition-all hover:scale-125">
+        {item.icon}
+      </div>
+      <AnimatePresence>
+        {isHovered && (
+          <motion.p
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            className="absolute left-12 top-0 w-32 text-sm text-zinc-600"
+          >
+            {item.name}
+          </motion.p>
+        )}
+      </AnimatePresence>
+    </a>
+  );
+};
+
+export const Nav = () => {
+  return (
+    <div className="sticky top-0 m-4 flex w-min flex-col space-y-4 rounded-full bg-slate-200 p-3">
+      {navItems.map((nav) => (
+        <NavItem item={nav} key={nav.name} />
+      ))}
+    </div>
+  );
+};
---
+++ src/shared/routes.ts
@@ -0,0 +1,6 @@
+export default {
+  home: "/",
+  newBooking: "/new-booking",
+  viewBookings: "/view-bookings",
+  contact: "/contact",
+} as const;
