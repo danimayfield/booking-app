4b589b946c2636d7da07ca90d63a19aef7cb923f
Dani Mayfield <dani@neon.cc>
2023-05-15 18:16:16 -0400

    style: create month picker to change date

--- src/features/viewBookings/MonthPicker.tsx
+++ src/features/viewBookings/MonthPicker.tsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { add, format, startOfMonth, sub } from "date-fns";
+import { add, startOfMonth, sub } from "date-fns";
 import { BsChevronLeft, BsChevronRight } from "react-icons/bs";
 import { useCalendarContext } from "./CalendarContext";
+import { DATE_PATTERN_MONTH, formatDate } from "@/shared/utils/formatters";
 
 export const MonthPicker = () => {
   const { activeDate, updateActiveDate } = useCalendarContext();
@@ -17,7 +18,7 @@ export const MonthPicker = () => {
     updateActiveDate(newDate);
   };
   return (
-    <div className="mb-6 flex w-full items-center justify-between">
+    <div className="mb-10 flex w-full items-center justify-between">
       <div className="flex text-slate-600">
         <button
           aria-label="Previous month"
@@ -26,28 +27,34 @@ export const MonthPicker = () => {
           <BsChevronLeft size="2rem" />
         </button>
       </div>
-      <div className="rounded-md bg-slate-300 px-4 py-2">
-        <label className="sr-only" htmlFor="month">
-          Select month
-        </label>
-        <select
-          id="month"
-          className="cursor-pointer bg-slate-300 text-white"
-          value={activeDate.getMonth() + 1}
-          onChange={e =>
-            handleChangeMonth(
-              new Date(`${currentYear}-${e.currentTarget.value}-1`),
-            )
-          }
-        >
-          {months.map(m => (
-            <option
-              key={m}
-              value={m}
-              label={format(new Date(`${currentYear}-${m}-1`), "MMMM")}
-            />
-          ))}
-        </select>
+      <div>
+        <p className="mb-1 text-center text-lg">{activeDate.getFullYear()}</p>
+        <div className="rounded-md bg-slate-300 px-4 py-2">
+          <label className="sr-only" htmlFor="month">
+            Select month
+          </label>
+          <select
+            id="month"
+            className="cursor-pointer bg-slate-300 text-white"
+            value={activeDate.getMonth() + 1}
+            onChange={e =>
+              handleChangeMonth(
+                new Date(`${currentYear}-${e.currentTarget.value}-1`),
+              )
+            }
+          >
+            {months.map(m => (
+              <option
+                key={m}
+                value={m}
+                label={formatDate(
+                  new Date(`${currentYear}-${m}-1`),
+                  DATE_PATTERN_MONTH,
+                )}
+              />
+            ))}
+          </select>
+        </div>
       </div>
       <div className="flex text-slate-600">
         <button
