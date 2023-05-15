1ba96c4b8a055f84aaf8e1162037db548c24abc9
Dani Mayfield <dani@neon.cc>
2023-05-15 11:58:14 -0400

    style: update web title name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "TTC Bookings",
+  title: "Booking App",
   description: "Application to manage businesses bookings",
 };
 
