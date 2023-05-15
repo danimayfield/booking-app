2986b9e36615e98ed42f200ab755737dc90ae7db
Dani Mayfield <dani@neon.cc>
2023-05-15 18:15:10 -0400

    feat: create booking details modal

---
+++ src/features/viewBookings/BookingDetailsModal.tsx
@@ -0,0 +1,66 @@
+import type { RefObject } from "react";
+import { useGetBookingData } from "./useGetBookingData";
+import { PageLoadingSpinner } from "@/shared/components";
+import { Modal } from "@/shared/components/Modal";
+import { formatDate } from "@/shared/utils/formatters";
+
+type ContactBookingDetailsModalProps = {
+  modalOverlayRef: RefObject<HTMLDivElement>;
+  onModalClose: () => void;
+  bookingId: string;
+};
+
+export const BookingDetailsModal = ({
+  modalOverlayRef,
+  onModalClose,
+  bookingId,
+}: ContactBookingDetailsModalProps) => {
+  const { data, error, isLoading } = useGetBookingData();
+  const booking = data?.find(b => b._id === bookingId);
+
+  if (booking === undefined) {
+    return (
+      <Modal modalOverlayRef={modalOverlayRef} onModalClose={onModalClose}>
+        <div className="w-96 px-4 py-12">
+          <p className="heading-lg mb-2">Booking not found...</p>
+          <p>Please try again later</p>
+        </div>
+      </Modal>
+    );
+  }
+
+  return (
+    <Modal modalOverlayRef={modalOverlayRef} onModalClose={onModalClose}>
+      {isLoading ? (
+        <div className="w-96 px-4 py-12">
+          <PageLoadingSpinner />
+        </div>
+      ) : (
+        <div className="flex w-full flex-col content-center items-center justify-center px-4 py-12 sm:w-96">
+          <p className="heading-lg">{booking.name}&apos;s Booking</p>
+          <div className="mt-6 space-y-2">
+            <p>{booking.description}</p>
+            <p>
+              Dates:&nbsp;
+              {formatDate(booking.startDateTime)}
+              &nbsp;-&nbsp;
+              {formatDate(booking.endDateTime)}
+            </p>
+            <p>
+              Email:&nbsp;
+              <a href={`mailto:${booking.email}`} className="link">
+                {booking.email}
+              </a>
+            </p>
+            <p>
+              Phone:&nbsp;
+              <a href={`tel:${booking.phone}`} className="link">
+                {booking.phone}
+              </a>
+            </p>
+          </div>
+        </div>
+      )}
+    </Modal>
+  );
+};
--- src/features/viewBookings/Calendar.tsx
+++ src/features/viewBookings/Calendar.tsx
@@ -1,32 +1,117 @@
-import React from "react";
-import { getDaysInMonth, getDay, startOfMonth } from "date-fns";
+import React, { useState } from "react";
+import {
+  getDaysInMonth,
+  getDay,
+  startOfMonth,
+  eachDayOfInterval,
+  isSameDay,
+} from "date-fns";
+import { BookingCondensed, DayItem } from "./DayItem";
 import { useGetBookingData } from "./useGetBookingData";
 import { useCalendarContext } from "./CalendarContext";
+import { BookingDetailsModal } from "./BookingDetailsModal";
+import { PageLoadingSpinner } from "@/shared/components";
+import { useModal } from "@/shared/components/Modal";
 
 export const Calendar = () => {
+  const [selectedBookingId, setSelectedBookingId] = useState("");
   const { data, error, isLoading } = useGetBookingData();
   const { activeDate } = useCalendarContext();
+  const { closeModal, modalOverlayRef, openModal } = useModal();
 
   const weekdayLabels = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
   const firstWeekdayOfMonth = getDay(startOfMonth(activeDate));
   const noOfDays = getDaysInMonth(activeDate);
   const monthArray = Array.from({ length: noOfDays }, (_, index) => index + 1);
+  const leadingDays = Array.from(
+    { length: firstWeekdayOfMonth },
+    (_, index) => index + 1,
+  );
+
+  const calendarColors = [
+    "#fde68a",
+    "#d9f99d",
+    "#a7f3d0",
+    "#99f6e4",
+    "#a5f3fc",
+    "#e2e8f0",
+    "#c7d2fe",
+    "#ddd6fe",
+    "#f5d0fe",
+    "#fecdd3",
+    "#e5e7eb",
+  ];
+
+  const condensedData: BookingCondensed[] | undefined = data?.map((d, i) => {
+    const colorIndex = i % calendarColors.length;
+    const color = calendarColors[colorIndex];
+
+    return {
+      id: d._id,
+      name: d.name,
+      color,
+      bookedDays: eachDayOfInterval({
+        start: new Date(d.startDateTime),
+        end: new Date(d.endDateTime),
+      }).map(bookedDate => {
+        if (isSameDay(bookedDate, new Date(d.startDateTime))) {
+          return new Date(d.startDateTime);
+        }
+        if (isSameDay(bookedDate, new Date(d.endDateTime))) {
+          return new Date(d.endDateTime);
+        }
+        return bookedDate;
+      }),
+    };
+  });
+
+  const handleModalClose = () => {
+    closeModal();
+    setSelectedBookingId("");
+  };
+
+  const handleOpenBookingDetails = (id: string) => {
+    setSelectedBookingId(id);
+    openModal();
+  };
+
+  if (isLoading) {
+    return <PageLoadingSpinner />;
+  }
 
   return (
-    <div className="grid min-w-min grid-cols-7 gap-1 overflow-auto">
-      {weekdayLabels.map(w => (
-        <div key={w}>{w}</div>
-      ))}
-      {monthArray.map((day, i) => (
-        <div
-          key={day}
-          className={`border border-solid border-gray-300 p-3 ${
-            i === 0 ? `col-start-${firstWeekdayOfMonth + 1}` : ""
-          }`}
-        >
-          {day}
-        </div>
-      ))}
-    </div>
+    <>
+      <div className="grid min-w-max grid-cols-7 gap-1 overflow-auto pr-6 lg:pr-0">
+        {weekdayLabels.map(w => (
+          <div key={w} className="text-center text-slate-700">
+            {w}
+          </div>
+        ))}
+        {leadingDays.map(e => (
+          <div key={e} />
+        ))}
+        {monthArray.map(day => {
+          const currentDate = new Date(
+            `${activeDate.getFullYear()}-${activeDate.getMonth() + 1}-${day}`,
+          );
+          const activeBookings = condensedData?.filter(item =>
+            item.bookedDays.some(d => isSameDay(d, currentDate)),
+          );
+          return (
+            <DayItem
+              key={day}
+              day={day}
+              activeBookings={activeBookings}
+              onOpenBookingDetails={handleOpenBookingDetails}
+            />
+          );
+        })}
+      </div>
+      <BookingDetailsModal
+        onModalClose={handleModalClose}
+        modalOverlayRef={modalOverlayRef}
+        bookingId={selectedBookingId}
+      />
+    </>
   );
 };
