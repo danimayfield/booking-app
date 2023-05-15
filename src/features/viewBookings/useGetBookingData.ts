41f28cf333878a488086326e97e33d14cab434f3
Dani Mayfield <dani@neon.cc>
2023-05-15 11:58:01 -0400

    feat: create useGetBookingData hook

---
+++ src/features/viewBookings/useGetBookingData.ts
@@ -0,0 +1,39 @@
+import { useEffect, useState } from 'react';
+import axios, { AxiosResponse } from 'axios';
+
+interface Booking {
+  _id: string;
+  title: string;
+  description: string;
+  createdAt: string;
+  email: string;
+  endDateTime: string;
+  name: string;
+  phone: string;
+  startDateTime: string;
+  updatedAt: string;
+}
+
+export const useGetBookingData = () => {
+  const [data, setData] = useState<Booking[] | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response: AxiosResponse<Booking[]> = await axios.get<Booking[]>(
+          'https://ttc-bookings-api.booker.tech/bookings'
+        );
+        setData(response.data);
+        setLoading(false);
+      } catch (error) {
+        setError(`Error fetching booking data: ${error}`);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return { data, loading, error };
+};
