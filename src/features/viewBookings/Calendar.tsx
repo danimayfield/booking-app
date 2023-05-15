b014627e15b88c242c279670d828413c30941b4f
Dani Mayfield <dani@neon.cc>
2023-05-15 18:38:53 -0400

    refactor: move constant variables to own separate folder

--- src/features/viewBookings/Calendar.tsx
+++ src/features/viewBookings/Calendar.tsx
@@ -10,16 +10,15 @@ import { BookingCondensed, DayItem } from "./DayItem";
 import { useGetBookingData } from "./useGetBookingData";
 import { useCalendarContext } from "./CalendarContext";
 import { BookingDetailsModal } from "./BookingDetailsModal";
-import { PageLoadingSpinner } from "@/shared/components";
+import { calendarColors, weekdayLabels } from "./constants";
 import { useModal } from "@/shared/components/Modal";
+import { PageLoadingSpinner } from "@/shared/components";
 
 export const Calendar = () => {
   const [selectedBookingId, setSelectedBookingId] = useState("");
   const { data, error, isLoading } = useGetBookingData();
   const { activeDate } = useCalendarContext();
   const { closeModal, modalOverlayRef, openModal } = useModal();
-
-  const weekdayLabels = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
   const firstWeekdayOfMonth = getDay(startOfMonth(activeDate));
   const noOfDays = getDaysInMonth(activeDate);
   const monthArray = Array.from({ length: noOfDays }, (_, index) => index + 1);
@@ -27,20 +26,6 @@ export const Calendar = () => {
     { length: firstWeekdayOfMonth },
     (_, index) => index + 1,
   );
-
-  const calendarColors = [
-    "#fde68a",
-    "#d9f99d",
-    "#a7f3d0",
-    "#99f6e4",
-    "#a5f3fc",
-    "#e2e8f0",
-    "#c7d2fe",
-    "#ddd6fe",
-    "#f5d0fe",
-    "#fecdd3",
-    "#e5e7eb",
-  ];
 
   const condensedData: BookingCondensed[] | undefined = data?.map((d, i) => {
     const colorIndex = i % calendarColors.length;
---
+++ src/features/viewBookings/constants.ts
@@ -0,0 +1,15 @@
+export const calendarColors = [
+  "#fde68a",
+  "#d9f99d",
+  "#a7f3d0",
+  "#99f6e4",
+  "#a5f3fc",
+  "#e2e8f0",
+  "#c7d2fe",
+  "#ddd6fe",
+  "#f5d0fe",
+  "#fecdd3",
+  "#e5e7eb",
+];
+
+export const weekdayLabels = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
