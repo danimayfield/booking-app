d1b9561c6ce752dc5e0b71c1f0b04927ab3384ac
Dani Mayfield <dani@neon.cc>
2023-05-16 14:48:29 -0400

    chore: update format date to include new pattern

--- src/shared/utils/formatters/formatDate.ts
+++ src/shared/utils/formatters/formatDate.ts
@@ -3,11 +3,12 @@ import { parseDate } from "./parseDate";
 
 export const DATE_PATTERN_MONTH = "MMMM";
 export const DATE_PATTERN_SHORT = "LLLL d, yyyy";
+export const DATE_PATTERN_DATE_TIME_FIELD = "yyyy-MM-dd'T'HH:mm";
 export const DATE_PATTERN_STANDARD = "LLL d, yyyy h:mm aaa";
 
 export function formatDate(
   date: string | Date,
-  pattern = DATE_PATTERN_STANDARD,
+  pattern: string | undefined = DATE_PATTERN_STANDARD,
 ) {
   const dateObj = typeof date === "string" ? parseDate(date) : date;
   return format(dateObj, pattern, { weekStartsOn: 1 });
