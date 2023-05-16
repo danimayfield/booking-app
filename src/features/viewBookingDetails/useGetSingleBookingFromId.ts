53ca137126e776d52e2511339f7e4c96e99eff0f
Dani Mayfield <dani@neon.cc>
2023-05-16 16:18:41 -0400

    chore: create single booking get request

--- src/features/viewBookingDetails/useGetSingleBookingFromId.ts
+++ src/features/viewBookingDetails/useGetSingleBookingFromId.ts
@@ -1,29 +1,17 @@
 import { useEffect, useState } from "react";
 import axios, { AxiosResponse } from "axios";
+import { Booking } from "../viewBookings";
 
-export interface Booking {
-  _id: string;
-  title: string;
-  description: string;
-  createdAt: string;
-  email: string;
-  endDateTime: string;
-  name: string;
-  phone: string;
-  startDateTime: string;
-  updatedAt: string;
-}
-
-export const useGetBookingData = () => {
-  const [data, setData] = useState<Booking[] | null>(null);
+export const useGetSingleBookingFromId = (bookingId: string) => {
+  const [data, setData] = useState<Booking | null>(null);
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response: AxiosResponse<Booking[]> = await axios.get<Booking[]>(
-          "https://ttc-bookings-api.booker.tech/bookings",
+        const response: AxiosResponse<Booking> = await axios.get<Booking>(
+          `https://ttc-bookings-api.booker.tech/bookings/${bookingId}`,
         );
         setData(response.data);
         setIsLoading(false);
@@ -33,7 +21,7 @@ export const useGetBookingData = () => {
     };
 
     fetchData();
-  }, []);
+  }, [bookingId]);
 
   return { data, isLoading, error };
 };
