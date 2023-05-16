9e9b091cf6f26d0cbf8675eb4978618fb3dde9fd
Dani Mayfield <dani@neon.cc>
2023-05-16 12:35:14 -0400

    chore: create form error components

---
+++ src/shared/components/CustomErrorMessage.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { twMerge } from "tailwind-merge";
+
+export type Errors = { [key: string]: string };
+
+type CustomErrorMessageProps = {
+  /** Name of field in error stack. */
+  name: string;
+  errors: Errors | undefined;
+  className?: string;
+};
+
+export const CustomErrorMessage = ({
+  name,
+  errors,
+  className,
+}: CustomErrorMessageProps) => {
+  const errorMessage = errors?.[name];
+
+  if (!errorMessage) {
+    return null;
+  }
+
+  return <p className={twMerge(`error ${className}`)}>{errorMessage}</p>;
+};
